0956abae604f35765f90a23e577262dbc18f3b05
kjayru <wiltinoco@gmail.com>
2019-08-13 01:58:18 -0500

    pasarela

--- resources/views/admin/paginas/miposicionpuntos/index.blade.php
+++ resources/views/admin/paginas/miposicionpuntos/index.blade.php
@@ -302,7 +302,7 @@
                                 </div>
                             </div>
                     <input type="hidden" name="paymentMethodId" value="" />
-                    <input type="hidden" name="transaction_amount">
+                    <input type="hidden" name="transaction_amount" value="20">
                     <input type="hidden" name="description" value="Pago por puntos">
                     <input type="hidden" name="project_id" value="">
                     <input type="hidden" name="user_id" value="{{ $user_id }}">
